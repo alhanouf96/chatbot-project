f6a91a132f4e464f8388662307d245f24c579790
allhanouf96 <allhanouf96@gmail.com>
2025-03-01 14:35:46 +0300

    stage two

--- chatbot.py
+++ chatbot.py
@@ -1,47 +1,61 @@
-from openai import OpenAI
 import streamlit as st
-import os 
-from dotenv import load_dotenv
+import requests
 
-# Set page title
 st.title("Chatbot basic")
 
-# Load environment variables
-load_dotenv()
+#FastApi take playload then sendit to openAI
+chat_url = "http://127.0.0.1:8000/chat/"
 
-# Create OpenAI client
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-# Set up session state
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Set up chat messages
 if "messages" not in st.session_state:
-    st.session_state.messages = [] 
-    # [{"role": "user" or "assistant", "content": "message"}, ...]
-
-# Display chat messages
+    st.session_state.messages = []
+ #st.session_state.messages تضاف لها المحادثه   
+#role assistant or end user
+#content answer or question
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Chat input
+#chat_message function in stremlit
+#markdown
+
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Get response from OpenAI
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
+
+#payload josn format
+#dict > list > dict
+        payload = {
+            "messages": [
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    # Display response
+            ]
+        } # chat history
+        #when send payload use headers
+        #notify API  what kind of payload that we will send =  json
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        # No Stream approach
+        #ارسال السؤال
+        stream = requests.post(chat_url, json=payload, headers=headers)
+         #طلب الرد فقط
+        response = stream.json()["reply"]
+        #عرضه في markdown
+        st.markdown(response)
+
+        # Stream approach
+        # def get_stream_response():
+        #     with requests.post(chat_url, json=payload, headers=headers, stream=True) as r:
+        #         for chunk in r:
+        #             yield chunk.decode('utf-8')
+        # response = st.write_stream(get_stream_response)
+
+    # حتى يعرض كل الحادثه مع الردود والاسئله
     st.session_state.messages.append({"role": "assistant", "content": response})
