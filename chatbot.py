074c8ec8e9ee9a476ee83d14453115accf28dd39
allhanouf96 <allhanouf96@gmail.com>
2025-03-20 23:57:16 +0300

    new

--- chatbot.py
+++ chatbot.py
@@ -154,7 +154,7 @@ with st.sidebar:
         st.button("Delete Chat", on_click=delete_chat)
 
 # Main Content
-st.title("Chatbot Application 6.3 newwww fainaly update")
+st.title("Chatbot Application 6.3 new hanouf")
 
 if st.session_state["current_chat"]:
     chat_id = st.session_state["current_chat"]
