c29eae44f39dec2225e172a02d9b76301e68c116
alhanouf96 <46896368+alhanouf96@users.noreply.github.com>
2025-05-19 13:09:53 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -113,6 +113,7 @@ load_chats_from_db()
 # st.write(st.session_state)
 # Sidebar
 with st.sidebar:
+    st.image("assets/sdalogo.png", use_container_width=True) 
     st.title("Chat Management")
 
     uploaded_pdf = st.file_uploader("Upload PDF", type="pdf", key="pdf_uploader")
